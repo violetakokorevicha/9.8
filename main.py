9e6f37449ff57af95737ae5cffc47468778ee4a4
violetakokorevi <91872388+violetakokorevicha@users.noreply.github.com>
2022-02-28 08:15:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import math
+for elem in range(1, 11):
+  n = (elem ** 3)
+  print(n)
+for elem in range(1, 16):
+  n = (elem ** 2)
+  print(n)
+for elem in range(1, 21):
+  n = math.sqrt(elem)
+  print (n)
+  
